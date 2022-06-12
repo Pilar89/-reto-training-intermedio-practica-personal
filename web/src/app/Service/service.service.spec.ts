cf3b712c5cd583aa6f8d3197483dec36800e40e2
pilar <ingeniera.coral@gmail.com>
2022-06-12 13:55:23 -0500

    jasmine pruebas de unidad

--- web/src/app/Service/service.service.spec.ts
+++ web/src/app/Service/service.service.spec.ts
@@ -2,12 +2,40 @@ import { TestBed } from '@angular/core/testing';
 
 import { ServiceService } from './service.service';
 
-xdescribe('ServiceService', () => {
-  it('should create', () => {
-    const mockAfauth = jasmine.createSpy() as any;
-    const mockStore = jasmine.createSpy() as any;
-    const mockRouter = jasmine.createSpy() as any;
-    const component = new ServiceService(mockAfauth, mockStore, mockRouter);
-    expect(component).toBeTruthy();
+describe('ServiceService', () => {
+  let mockAfauth: any;
+  let mockStore: any;
+  let mockRouter: any;
+  let service: ServiceService;
+
+  beforeEach(() => {
+    mockAfauth = {
+      authState: { subscribe: jasmine.createSpy() },
+      signInWithEmailAndPassword: jasmine.createSpy(),
+      signOut: jasmine.createSpy(),
+    } as any;
+    mockStore = jasmine.createSpy() as any;
+    mockRouter = jasmine.createSpy() as any;
+    service = new ServiceService(mockAfauth, mockStore, mockRouter);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+    expect(mockAfauth.authState.subscribe).toHaveBeenCalled();
+  });
+
+  it('login', () => {
+    const user = 'user';
+    const passwd = 'passwd';
+    service.login(user, passwd);
+    expect(mockAfauth.signInWithEmailAndPassword).toHaveBeenCalledWith(
+      user,
+      passwd
+    );
+  });
+
+  it('logout', () => {
+    service.logout();
+    expect(mockAfauth.signOut).toHaveBeenCalled();
   });
 });
--- web/src/app/app.component.spec.ts
+++ web/src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-xdescribe('AppComponent', () => {
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -20,14 +20,5 @@ xdescribe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('reto_sofka');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'reto_sofka app is running!'
-    );
   });
 });
